7e5fdb7adca4381d2b61a4f719d5c02186c4ad59
TimG <tim.goransson@gmail.com>
2016-03-22 09:19:23 +0100

    GameObject och MovableGameObject classer skapade.

---
+++ Underground/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Underground
+{
+    abstract class GameObject
+    {
+        public GameObject()
+        {
+
+        }
+
+        public void Update() 
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+
+      
+    }
+}
---
+++ Underground/MovableGameObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Underground
+{
+    abstract class MovableGameObjects : GameObject
+    {
+        public MovableGameObjects() : base()
+        {
+
+        }
+
+        public void Update()
+        {
+         
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
